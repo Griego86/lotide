332af02274eb88319ee3e0bddaeea8267d8087b4
Griego86 <dimi1086@hotmail.com>
2023-09-30 17:08:51 -0700

    Formatted and added different test code.

--- without.js
+++ without.js
@@ -1,28 +1,41 @@
-const without = function(source, itemsToRemove) {
-  let results = [];
-  for (let i = 0; i < source.length; i++) {
-    if (!source[i].includes(itemsToRemove)) {
-      results.push(source[i]);
+// eqArraus function
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
+  } return true;
+};
+
+//assertArraysEqual function
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`😀😀😀 Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`😥😥😥 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
-const assertArraysEqual = function (array1, array2) {
-  const eqArrays = function (array1, array2) {
-    if (array1.length !== array2.length) {
-      return false;
-    } for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        return true;
-      }
-    } return false;
-  }; const message = eqArrays(array1, array2)
-    ? `😀😀😀 Arrays Equal Assertion Passed: ${array1} === ${array2}`
-    : `😥😥😥 Arrays Equal Assertion Failed: ${array1} !== ${array2}`;
-  console.log(message);
+
+// // Actual function
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== itemsToRemove[i]) {
+      newArray.push(source[i]);
+    }
+  } return newArray;
 };
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// Actual function refactored
+/*const without = function (source, itemsToRemove) {
+  let newArray = source.filter(item => !itemsToRemove.includes(item));
+  return newArray;
+};*/
+
+
+// Test code
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // should PAS
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // should PASS
