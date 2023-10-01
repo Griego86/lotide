f176a3b079295ce64c9c5523bd6fd2fffc92a8e3
Griego86 <dimi1086@hotmail.com>
2023-09-30 18:24:17 -0700

    Formatted and added different test code.

--- middle.js
+++ middle.js
@@ -1,41 +1,35 @@
-// TEST/ASSERTION FUNCTIONS
-const assertArraysEqual = function (array1, array2) {
-  const eqArrays = function (array1, array2) {
-    if (array1.length !== array2.length) {
-      return false;
-    } for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        return true;
-      }
-    } return false;
-  }; const message = eqArrays(array1, array2)
-    ? `😀😀😀 Arrays Equal Assertion Passed: ${array1} === ${array2}`
-    : `😥😥😥 Arrays Equal Assertion Failed: ${array1} !== ${array2}`;
-  console.log(message);
-};
-
-const eqArrays = function (array1, array2) {
+// eqArrays function
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   } for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true;
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-  } return false;
+  } return true;
 };
 
-// ACTUAL FUNCTION
-const middle = function (array) {
-  const size = array.length;
-  if (size <= 2) {
-    return [];
-  } const middleIndex = Math.floor(size / 2);
-  if (size % 2 === 0) {
-    return array.slice(middleIndex - 1, middleIndex + 1);
+// assertArraysEqual function
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`😀😀😀 Assertion Passed: ${array1} === ${array2}`);
   } else {
-    return [array[middleIndex]];
+    console.log(`😥😥😥 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
-// TEST CODE
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4, 5]);
+// Actual function
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } const sliceArgument = Math.floor(array.length / 2);
+  if (array.length % 2 === 0) {
+    return array.slice(sliceArgument - 1, (sliceArgument + 1));
+  } else {
+    return array[Math.ceil(sliceArgument)];
+  }
+};
+
+console.log(middle([1, 2, 3, 4, 5, 6])); // should return [3, 4]
+assertArraysEqual(middle([1, 2, 3]), 2); // should PASS
+console.log(middle([1, 2, 3, 4, 5])); // should return 3
