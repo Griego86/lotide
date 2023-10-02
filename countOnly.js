f1a7c1be121e6c7ffe6df3dfe0fdb9197257eb45
Griego86 <dimi1086@hotmail.com>
2023-10-01 18:46:21 -0700

    countOnly will be given an array and an object. It will return an object containing counts of everything that the input object listed.

---
+++ countOnly.js
@@ -0,0 +1,47 @@
+// asserrtEqual function
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😥😥😥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// Actual function
+const countOnly = function (allItems, itemsToCount) {
+  let results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+        console.log(results);
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+
+// Test code
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
