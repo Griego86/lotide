e8efe45195eda560d53b24bfc3072d911b4845c6
Griego86 <dimi1086@hotmail.com>
2023-10-03 11:31:54 -0700

    Function which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.

---
+++ findKey.js
@@ -0,0 +1,32 @@
+// Test function
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😥😥😥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Actual function
+const findKey = (object, callback) => {
+  for (item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  } return undefined;
+};
+
+// Test code
+const testObject = {
+  "Blue Hill": {stars: 1},
+  "Akaleri": {stars: 3},
+  "noma": {stars: 2},
+  "elBulli": {stars: 3},
+  "Ora": {stars: 2},
+  "Akelarre": {stars: 3}
+};
+
+assertEqual(findKey(testObject, x => x.stars === 2), "noma") // should PASS
+assertEqual(findKey(testObject, x => x.stars > 2), "Akaleri") // should PASS
+assertEqual(findKey(testObject, x => x.stars < 2), "Blue Hill") // should PASS
+assertEqual(findKey(testObject, x => x.stars > 3), undefined) // should PASS
