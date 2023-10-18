94545ae6a67eb6a601190887ecdd903a0f3bf51c
Griego86 <dimi1086@hotmail.com>
2023-10-18 12:08:04 -0700

    Refactored function

--- middle.js
+++ middle.js
@@ -1,11 +1,16 @@
-const middle = function(array) {
-  if (array.length <= 2) {
+const middle = (array) => {
+  const length = array.length;
+
+  if (length <= 2) {
     return [];
-  } const sliceArgument = Math.floor(array.length / 2);
-  if (array.length % 2 === 0) {
-    return array.slice(sliceArgument - 1, (sliceArgument + 1));
+  } 
+  
+  const middleIndex = Math.floor(length / 2); // Rounds down
+  
+  if (length % 2 === 0) {
+    return array.slice(middleIndex - 1, middleIndex + 1);
   } else {
-    return array[Math.ceil(sliceArgument)];
+    return array[middleIndex];
   }
 };
 
